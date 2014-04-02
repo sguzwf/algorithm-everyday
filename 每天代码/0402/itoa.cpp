d536ad9b2dae27038321caa09d0f1b4cdb1533c0
bruceren8 <baobinren@gmail.com>
2014-04-02 16:57:08 +0800

    update 0402/itoa.cpp

--- 每天代码/0402/itoa.cpp
+++ 每天代码/0402/itoa.cpp
@@ -2,8 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* _itoa(int value, char *str, int base)
+char* itoa(int value, char *str, int base)
 {
+    if (str == NULL) return NULL;
+    int flag = 0;
+    if (value < 0 && base == 10)//处理10进制负数
+    {
+        flag = 1;
+        value = -value;
+    }
+    else if (value < 0)
+        return NULL;
+    int len = 0;//字符串的长度
+    while (value)
+    {
+        str[len++] = '0' + value%base;
+        value /= base;
+    }
+    if (flag)
+        str[len++] = '-';
+    str[len] = '\0';
+    for (int i = 0; i < len/2; i++)//将字符串逆置
+    {
+        char t = str[i];
+        str[i] = str[len-1-i];
+        str[len-1-i] = t;
+    }
+    return str;
 }
 
 int main()
@@ -21,6 +46,24 @@ int main()
     memset(str, 0, sizeof(str));
     itoa(i, str, 2);
     if (strcmp(str, "1111011") == 0) printf("123 2 success!\n");
+    else printf("err:%s\n", str);
+
+    i = 123;
+    memset(str, 0, sizeof(str));
+    itoa(i, str, 8);
+    if (strcmp(str, "173") == 0) printf("123 8 success!\n");
+    else printf("err:%s\n", str);
+
+    i = -123;
+    memset(str, 0, sizeof(str));
+    itoa(i, str, 10);
+    if (strcmp(str, "-123") == 0) printf("-123 10 success!\n");
+    else printf("err:%s\n", str);
+
+    i = -123;
+    memset(str, 0, sizeof(str));
+    itoa(i, str, 2);
+    if (strcmp(str, "") == 0) printf("-123 2 success!\n");
     else printf("err:%s\n", str);
     return 0;
 }
